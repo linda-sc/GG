1788a64414d3b4face724b627ca06d01908c79f7
Linda Chen <lindachenny@gmail.com>
2020-01-17 14:12:44 -0500

    voting started

---
+++ Goonz Gamez/Answerz/AnswerCell.swift
@@ -0,0 +1,21 @@
+//
+//  WaitingCell.swift
+//  Goonz Gamez
+//
+//  Created by Linda Chen on 1/16/20.
+//  Copyright © 2020 Synestha. All rights reserved.
+//
+
+import UIKit
+
+class AnswerCell: UICollectionViewCell {
+
+    @IBOutlet weak var answerLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
--- Goonz Gamez/Answerz/VotingVC.swift
+++ Goonz Gamez/Answerz/VotingVC.swift
@@ -10,68 +10,47 @@ import UIKit
 
 class VotingVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
 
-    @IBOutlet weak var waitingCollection: UICollectionView!
-
-    var waitingList = Question().goonz
-    var numberOfPeople = Question().goonz.count
-    
-    var counter = 0
-    var timer = Timer()
-
-    @IBOutlet weak var progressView: UIProgressView!
+    @IBOutlet weak var answerCollection: UICollectionView!
     @IBOutlet weak var progressContainer: UIView!
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    var sampleAnswers = [
+    "daniel, but not for the ride",
+    "positions other than missionary",
+    "blockchain",
+    "his secret fantasy of a threesome with daniel and spencer except he's only watching",
+    "bagel holes",
+    "bullshit about being a CEO and World Class Fencer, which works incredibly well on insecure small Asian girls",
+    "(1) condoms for harvard men that she isn't going to use, (2) a machine learning textbook that she isn't going to read, (3) her iPhone with Hinge, Bumble, and Tinder on it.",
+    "Adderall, fuccbois, and hair growth pills",
+    "Condoms, plan B, and then probably a scale fo she can check her weight when she feel insecure about a guy not responding.",
+    "A Harvard deferral letter",
+    "His mother's pyramid scheme involving vitamins and Melissa",
+    "Report her family to ICE to avoid paying child support",
+    "colonize"
+    ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         progressContainer.layer.cornerRadius = 5
         progressContainer.clipsToBounds = true
         progressView.clipsToBounds = true
-        waitingCollection.delegate = self
-        waitingCollection.dataSource = self
-        waitingCollection.register(UINib(nibName: "WaitingCell", bundle: nil), forCellWithReuseIdentifier: "WaitingCell")
+        answerCollection.delegate = self
+        answerCollection.dataSource = self
+        answerCollection.register(UINib(nibName: "AnswerCell", bundle: nil), forCellWithReuseIdentifier: "AnswerCell")
         
         
         //Set estimated item size
-        if let flow = waitingCollection.collectionViewLayout as? UICollectionViewFlowLayout {
+        if let flow = answerCollection.collectionViewLayout as? UICollectionViewFlowLayout {
             flow.estimatedItemSize = CGSize(width: 1, height: 1)
         }
         
         //Disappearing cells
         let flowLayout = BouncyLayout(style: .prominent)
-        self.waitingCollection.setCollectionViewLayout(flowLayout, animated: true)
+        self.answerCollection.setCollectionViewLayout(flowLayout, animated: true)
     
         checkIfDone()
     }
-    
-    
-    // MARK: - Removing people
-    @IBAction func screenTapped(_ sender: Any) {
-        triggerAction()
-        checkIfDone()
-    }
-    
-    @objc func triggerAction(){
-        print("Removing random person")
-        if self.waitingList.count > 0 {
-            let random = Int.random(in: 0...self.waitingList.count - 1)
-            self.removeFromWaitTable(row: random)
-        }
-    }
-
-    func removeFromWaitTable(row: Int) {
-        self.waitingList.remove(at: row)
-        let myNSIndexPath = [NSIndexPath(row: row, section: 0)]
-        self.waitingCollection.deleteItems(at: myNSIndexPath as [IndexPath])
-        
-        let peopleRemaining = Float(self.waitingList.count)
-        let peopleFinished = Float(numberOfPeople) - peopleRemaining
-        let total = Float(numberOfPeople)
-        print("People finished: \(peopleFinished)")
-        let progress: Float = peopleFinished/total
-        print("Progress: \(progress)")
-           progressView.setProgress(progress, animated: true)
-    }
-    
 
        
     private func setStructure(for cell: UICollectionViewCell) {
@@ -82,7 +61,7 @@ class VotingVC: UIViewController, UICollectionViewDelegate, UICollectionViewData
     
     // MARK: - Checking to see if everyone is done
     func checkIfDone() {
-        if self.waitingList.isEmpty {
+        if false {
             self.performSegue(withIdentifier: "GoToVoting", sender: self)
             print("GoToVoting segue triggered.")
         }
@@ -92,20 +71,35 @@ class VotingVC: UIViewController, UICollectionViewDelegate, UICollectionViewData
     // MARK: - Collection view data source
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.waitingList.count
+        return self.sampleAnswers.count
 
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = waitingCollection.dequeueReusableCell(withReuseIdentifier: "WaitingCell", for: indexPath) as! WaitingCell
+        let cell = answerCollection.dequeueReusableCell(withReuseIdentifier: "AnswerCell", for: indexPath) as! AnswerCell
                self.setStructure(for: cell)
-        cell.nameLabel.text = self.waitingList[indexPath.row]
+        cell.answerLabel.text = self.sampleAnswers[indexPath.row]
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let width = view.bounds.width - 5
-        let height: CGFloat = 40
+        let width = view.bounds.width - 60
+        let text = self.sampleAnswers[indexPath.row]
+        let height: CGFloat = estimateFrameForText(text: text).height + 20
         return CGSize(width: width, height: height)
         }
+    
+    // MARK: - Estimate cell height
+    func estimateFrameForText(text: String) -> CGRect {
+        //we make the height arbitrarily large so we don't undershoot height in calculation
+        let height: CGFloat = 1000
+        //Set the width to the width of the text in the cell.
+        let width: CGFloat = view.bounds.width - 80
+        
+        let size = CGSize(width: width, height: height)
+        let options = NSStringDrawingOptions.usesFontLeading.union(.usesLineFragmentOrigin)
+        let attributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20, weight: UIFont.Weight.bold)]
+        
+        return NSString(string: text).boundingRect(with: size, options: options, attributes: attributes, context: nil)
+    }
 }
